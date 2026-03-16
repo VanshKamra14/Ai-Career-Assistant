98273bbf40f640c967450f233080f9af31ccf678
vanshkamra14 <22bec108@nith.ac.in>
2026-03-16 18:47:13 +0530

    Url update

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,8 +1,9 @@
+const BASE_URL = import.meta.env.VITE_URL || "http://localhost:3000";
 import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: BASE_URL,
     withCredentials: true
 })
 
