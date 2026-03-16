6d8f6722521be29dd537762d17cbca42b489b397
vanshkamra14 <22bec108@nith.ac.in>
2026-03-16 18:23:04 +0530

    first commit

---
+++ Backend/src/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+async function connectToDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB Connected');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+    }
+}
+
+module.exports = connectToDB;
